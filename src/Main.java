97541a1b2da2391ee68b2fb7bbfb4068a80e7e7d
Van Hoang <hoangray2309@gmail.com>
2025-02-18 23:42:33 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Book book= new Book(23,"HTB","java",2024);
+        book.setId(29);
+        System.out.println(book.getId());
+
+
+    }
+}
