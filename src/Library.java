79d5325321f45dedf2eb9a1fc3f436117aa296f5
vanhoang <hoangray2309@gmail.com>
2025-02-19 09:28:54 +0700

    new commit

--- src/Library.java
+++ src/Library.java
@@ -1,7 +1,32 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
     private List<Book> books;
 
-   
+    public Library() {
+        this.books = new ArrayList<Book>();
+    }
+    //add
+    public void addBook(Book book) {
+        books.add(book);
+        System.out.println("Đã thêm sách: "+book.getTitle());
+    }
+   //delete
+    public void deleteBook(int id){
+        Book bookremove= null;
+        for(Book book:books){
+            if(book.getId()==id){
+                bookremove=book;
+                break;
+            }
+        }
+        if(bookremove!= null){
+            books.remove(bookremove);
+
+        System.out.println("Đã xóa sách: "+bookremove.getTitle()+"ID: "+bookremove.getId());
+        } else{
+            System.out.println("Không tìm thấy id");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
         Book book= new Book(23,"HTB","java",2024);
         System.out.println(book);
 
+        Library library = new Library();
+        library.addBook(new Book(23,"HTB","java",2024));
+
 
     }
 }
